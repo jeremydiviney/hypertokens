61163149d0c959cb06fe4aba8d3fe62e4df786b5
Jeremy Diviney <jeremydiviney@gmail.com>
2025-03-12 12:51:22 -0700

    update exp

--- jpt1_train.py
+++ jpt1_train.py
@@ -760,7 +760,7 @@ if __name__ == "__main__":
         "token_space_dim": [768],
         "epochs": [1],
         "batch_size": [24],
-        "lr": [0.008, 0.008, 0.008],
+        "lr": [0.0008, 0.0008, 0.0008],
         "num_head": [12],
         "n_layers": [12],
         "jpt_embed_dim": [768],
